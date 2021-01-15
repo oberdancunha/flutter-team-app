0be84704941a749eb6f319a21c82c6ea805ea263
oberdancunha <oberdancunha@gmail.com>
2021-01-14 21:06:29 -0300

    Integration test commit.

--- test_integration/team_result_test.dart
+++ test_integration/team_result_test.dart
@@ -3,8 +3,13 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:teamapp/app_module.dart';
 import 'package:teamapp/config_reader.dart';
+import 'package:teamapp/domain/teams/i_team_data_source.dart';
+import 'package:teamapp/domain/teams/team.dart';
+import 'package:teamapp/infrastructure/team/team_data_source_dio.dart';
+import 'package:teamapp/infrastructure/team/team_dto.dart';
 import 'package:teamapp/presentation/core/app_widget.dart';
 import 'package:teamapp/presentation/team/widget/team_details.dart';
 import 'package:teamapp/presentation/team/widget/team_form.dart';
@@ -13,12 +18,24 @@ import 'package:teamapp/presentation/team/widget/team_not_found.dart';
 import 'package:teamapp/presentation/team/widget/team_result.dart';
 import 'package:teamapp/presentation/team/widget/team_result_details.dart';
 
+class MockTeamDataSource extends Mock implements TeamDataSourceDio {}
+
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   const teamSearchTextFieldKey = 'team_search_text';
+  MockTeamDataSource mockTeamDataSource;
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  final teamJson = TeamDto.fromJson({
+    "id": 126,
+    "name": "Sao Paulo",
+    "country": "Brasil",
+    "founded": 1930,
+    "logo": "https://media.api-sports.io/football/teams/126.png"
+  }).toDomain();
 
   setUp(() async {
     await ConfigReader.initialize();
+    mockTeamDataSource = MockTeamDataSource();
   });
 
   Future<void> searchResult({
@@ -27,7 +44,13 @@ void main() {
     @required int pumpMilliseconds,
     @required Function body,
   }) async {
-    initModule(AppModule(), initialModule: true);
+    initModule(
+      AppModule(),
+      initialModule: true,
+      changeBinds: [
+        Bind<ITeamDataSource>((i) => mockTeamDataSource),
+      ],
+    );
     await tester.pumpWidget(AppWidget());
     await tester.enterText(
       find.byKey(const Key(teamSearchTextFieldKey)),
@@ -47,6 +70,9 @@ void main() {
   testWidgets(
     'Should return an alert when the team searched is not found',
     (WidgetTester tester) async {
+      when(mockTeamDataSource.getDetails(any)).thenAnswer(
+        (_) async => Team.empty(),
+      );
       await searchResult(
         tester: tester,
         inputText: 'Sao Paul',
@@ -62,6 +88,9 @@ void main() {
   testWidgets(
     'Should return the data when the team searched is found',
     (WidgetTester tester) async {
+      when(mockTeamDataSource.getDetails(any)).thenAnswer(
+        (_) async => teamJson,
+      );
       await searchResult(
         tester: tester,
         inputText: 'Sao Paulo',
